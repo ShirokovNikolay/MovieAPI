ac3acae4c50820e972db3efa61646be9a9476840
Nikolay <shirkol2006@mail.ru>
2026-03-22 19:19:58 +0300

    Create template for genre and movie schemas.

---
+++ app/schemas/genre.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel
+
+
+class GenreBase(BaseModel):
+    """
+    Базовый класс для работы с жанром фильма.
+    """
---
+++ app/schemas/movie.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel
+
+
+class MovieBase(BaseModel):
+    """
+    Базовый класс для работы с фильмом.
+    """
