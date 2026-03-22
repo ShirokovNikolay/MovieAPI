eceb4369710dc4fbe5513c64c4c398b25cc1bad0
Nikolay <shirkol2006@mail.ru>
2026-03-22 13:35:37 +0300

    Create config to use database.

--- app/config.py
+++ app/config.py
@@ -1,10 +1,30 @@
+from pathlib import Path
+
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
+class DataBaseConfig(BaseModel):
+    user: str
+    password: str
+    host: str = "localhost"
+    port: int = 5432
+    db_name: str
+    echo: bool = False
+
+    @property
+    def url_database(self) -> str:
+        return f"postgresql+psycopg://{self.user}:{self.password}@{self.host}:{self.port}/{self.db_name}"
+
+
 class Settings(BaseSettings):
-    debug: bool = True
+    BASE_DIR: Path = Path(__file__).parent
+    database: DataBaseConfig
+    debug: bool
+
     model_config: SettingsConfigDict = SettingsConfigDict(
-        env_file="./env",
+        env_file=BASE_DIR / ".env",
+        env_nested_delimiter="__",
     )
 
 
