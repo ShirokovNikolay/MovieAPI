0335a260733ed5017964d9d6336b88a715f1bd77
Nikolay <shirkol2006@mail.ru>
2026-03-22 11:42:06 +0300

    Run app.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,28 @@
-def main():
-    print("Hello from movieapi!")
+import uvicorn
+from fastapi import FastAPI, Request
+from config import settings
+
+app = FastAPI(
+    title="Movie API",
+    debug=settings.debug,
+)
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def read_root(
+    request: Request,
+    name: str = "Nikolay",
+):
+    docs_url = request.url.replace(
+        path="/docs",
+        query="",
+    )
+    return {
+        "message": f"Hello {name}!",
+        "docs_url": str(docs_url),
+    }
+
+
+@app.get("/health")
+def check_health():
+    return {"status": "ok"}
