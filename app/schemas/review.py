3fa689e0e243eed1e739092e0a85ec8d613e2c4a
Nikolay <shirkol2006@mail.ru>
2026-03-23 11:08:18 +0300

    Create review schemas.

---
+++ app/schemas/review.py
@@ -0,0 +1,53 @@
+from typing import Annotated
+
+from annotated_types import MaxLen
+from pydantic import BaseModel, Field
+from datetime import datetime
+
+
+StringMaxLength400 = Annotated[str, MaxLen(max_length=400)]
+RatingConstarint = Annotated[
+    int,
+    Field(ge=0, le=10),
+]
+
+
+class ReviewBase(BaseModel):
+    """
+    Базовая модель для работы с отзывами.
+    """
+
+    review_text: StringMaxLength400
+    rating: RatingConstarint
+
+
+class ReviewCreate(ReviewBase):
+    """
+    Модель для создания отзыва о фильме.
+    """
+
+    movie_id: int
+
+
+class ReviewUpdate(ReviewBase):
+    """
+    Модель для обновления отзыва о фильме.
+    """
+
+
+class ReviewPartialUpdate(ReviewBase):
+    """
+    Модель для частичного обновления отзыва о фильме.
+    """
+
+    review_text: StringMaxLength400 | None = None
+    rating: RatingConstarint | None = None
+
+
+class ReviewResponse(ReviewBase):
+    """
+    Модель для вывода информации об отзыве.
+    """
+
+    id: int
+    publication_date: datetime
