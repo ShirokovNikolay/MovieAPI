bc0762a03caeb5018e71f4cf7a384e310b4f06ba
Nikolay <shirkol2006@mail.ru>
2026-03-23 10:46:48 +0300

    Create user schemas.

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,7 +1,67 @@
+from typing import Annotated
+
+from annotated_types import MaxLen
 from pydantic import BaseModel
+
+
+StringMaxLength255 = Annotated[
+    str,
+    MaxLen(maxLength=255),
+]
+
+StringMaxLength30 = Annotated[
+    str,
+    MaxLen(maxLength=30),
+]
+
+StringMaxLength20 = Annotated[
+    str,
+    MaxLen(maxLength=20),
+]
 
 
 class UserBase(BaseModel):
     """
     Базовая модель для пользователя.
     """
+
+    surname: StringMaxLength30
+    name: StringMaxLength20
+    login: StringMaxLength20
+    email: StringMaxLength255
+
+
+class UserCreate(UserBase):
+    """
+    Модель для создания пользователя.
+    """
+
+    password: StringMaxLength20
+
+
+class UserUpdate(UserBase):
+    """
+    Модель для обновления данных о пользователе
+    """
+
+    password: StringMaxLength20
+
+
+class UserPartialUpdate(UserBase):
+    """
+    Модель для частичного обновления данных о пользователе.
+    """
+
+    surname: StringMaxLength30 | None = None
+    name: StringMaxLength20 | None = None
+    login: StringMaxLength20 | None = None
+    email: StringMaxLength255 | None = None
+    password: StringMaxLength20 | None = None
+
+
+class UserResponse(UserBase):
+    """
+    Модель для вывода информации о пользователе.
+    """
+
+    id: int
