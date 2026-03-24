e14f02709abb8e2273b1e8bf31e76528240bdc60
Nikolay <shirkol2006@mail.ru>
2026-03-24 13:26:56 +0300

    Add repositories to init file.

---
+++ app/repositories/__init__.py
@@ -0,0 +1,11 @@
+from .genre import GenreRepository
+from .movie import MovieRepository
+from .review import ReviewRepository
+from .user import UserRepository
+
+__all__ = (
+    "GenreRepository",
+    "MovieRepository",
+    "ReviewRepository",
+    "UserRepository",
+)
