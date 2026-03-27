1f2791e76651b69fe8f6907e5d2e8fc8a5287910
Nikolay <shirkol2006@mail.ru>
2026-03-27 17:26:51 +0300

    Add jwt utils.

--- app/security.py
+++ app/security.py
@@ -1,4 +1,7 @@
 import bcrypt
+import jwt
+
+from config import settings
 
 
 def hash_password(password: str) -> str:
@@ -11,3 +14,27 @@ def verify_password(password: str, hashed_password: str) -> bool:
     password_bytes = password.encode()
     hashed_password_bytes = hashed_password.encode()
     return bcrypt.checkpw(password_bytes, hashed_password_bytes)
+
+
+def encode_jwt(
+    payload: dict,
+    secret_key: str = settings.auth_jwt.secret_key,
+    algorithm: str = settings.auth_jwt.algorithm,
+) -> str:
+    return jwt.encode(
+        payload,
+        secret_key,
+        algorithm=algorithm,
+    )
+
+
+def decode_jwt(
+    token,
+    secret_key: str = settings.auth_jwt.secret_key,
+    algorithm: str = settings.auth_jwt.algorithm,
+) -> dict:
+    return jwt.decode(
+        token,
+        secret_key,
+        algorithms=[algorithm],
+    )
