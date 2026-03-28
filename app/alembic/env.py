6caded517edfcff5f151373f8440d0e21c4eb0b2
Nikolay <shirkol2006@mail.ru>
2026-03-28 21:52:33 +0300

    Fix important alembic bug.

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -20,6 +20,8 @@ if config.config_file_name is not None:
 # target_metadata = mymodel.Base.metadata
 from database import Base
 from core.config import settings
+from models import *
+
 
 target_metadata = Base.metadata
 
