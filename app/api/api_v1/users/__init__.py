3e6b729a816bd61a6f290b667295e9c849d9ee51
Nikolay <shirkol2006@mail.ru>
2026-03-28 19:02:13 +0300

    Add auth views using profile.

--- app/api/api_v1/users/__init__.py
+++ app/api/api_v1/users/__init__.py
@@ -3,11 +3,13 @@ from fastapi import APIRouter
 from .list_views import router as list_users_router
 from .details_views import router as details_users_router
 from .details_views_login import router as details_users_router_login
+from .profile_views import router as profile_users_router
 
 router = APIRouter(
     tags=["Users"],
     prefix="/users",
 )
+router.include_router(profile_users_router)
 router_by_login = APIRouter(prefix="/by-login")
 router_by_login.include_router(details_users_router_login)
 router.include_router(list_users_router)
---
+++ app/api/api_v1/users/profile_views.py
@@ -0,0 +1,86 @@
+from typing import Annotated
+
+from fastapi import APIRouter, status, Depends
+from dependencies.auth import get_current_user_id_by_access_token_payload
+from dependencies.services import get_user_service
+from schemas.user import (
+    UserResponse,
+    UserUpdate,
+    UserPartialUpdate,
+)
+from services import UserService
+
+router = APIRouter(prefix="/me")
+
+
+@router.get(
+    "/",
+    response_model=UserResponse,
+    status_code=status.HTTP_200_OK,
+)
+def get_user_profile(
+    current_user_id: Annotated[
+        int,
+        Depends(get_current_user_id_by_access_token_payload),
+    ],
+    user_service: Annotated[
+        UserService,
+        Depends(get_user_service),
+    ],
+):
+    return user_service.get_user_by_id(current_user_id)
+
+
+@router.put(
+    "/",
+    response_model=UserResponse,
+    status_code=status.HTTP_200_OK,
+)
+def update_user_profile(
+    update_data: UserUpdate,
+    current_user_id: Annotated[
+        int,
+        Depends(get_current_user_id_by_access_token_payload),
+    ],
+    user_service: Annotated[
+        UserService,
+        Depends(get_user_service),
+    ],
+):
+    return user_service.update_user(current_user_id, update_data)
+
+
+@router.patch(
+    "/",
+    response_model=UserResponse,
+    status_code=status.HTTP_200_OK,
+)
+def partial_update_user_profile(
+    update_data: UserPartialUpdate,
+    current_user_id: Annotated[
+        int,
+        Depends(get_current_user_id_by_access_token_payload),
+    ],
+    user_service: Annotated[
+        UserService,
+        Depends(get_user_service),
+    ],
+):
+    return user_service.partial_update_user(current_user_id, update_data)
+
+
+@router.delete(
+    "/",
+    status_code=status.HTTP_204_NO_CONTENT,
+)
+def delete_user_profile(
+    current_user_id: Annotated[
+        int,
+        Depends(get_current_user_id_by_access_token_payload),
+    ],
+    user_service: Annotated[
+        UserService,
+        Depends(get_user_service),
+    ],
+):
+    user_service.delete_user_by_id(current_user_id)
