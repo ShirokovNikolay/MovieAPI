38b1573aee1a24c76782ee5b4df2fd604b6fa79b
Nikolay <shirkol2006@mail.ru>
2026-03-29 18:04:35 +0300

    Add UserRole enum class.

--- app/core/constants.py
+++ app/core/constants.py
@@ -1,3 +1,10 @@
+from enum import Enum
+
 TOKEN_TYPE = "type"
 ACCESS_TOKEN_TYPE = "access"
 REFRESH_TOKEN_TYPE = "refresh"
+
+
+class UserRole(str, Enum):
+    user: str = "user"
+    admin: str = "admin"
