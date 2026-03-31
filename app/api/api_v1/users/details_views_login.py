47019f723848323908ae63cda3bd2c39be2076e1
Nikolay <shirkol2006@mail.ru>
2026-03-31 23:49:31 +0300

    Add async to user views.

--- app/api/api_v1/users/details_views_login.py
+++ app/api/api_v1/users/details_views_login.py
@@ -18,14 +18,14 @@ router = APIRouter(
     response_model=UserResponse,
     status_code=status.HTTP_200_OK,
 )
-def get_user_by_login(
+async def get_user_by_login(
     login: str,
     user_service: Annotated[
         UserService,
         Depends(get_user_service),
     ],
 ):
-    return user_service.get_user_by_login(login)
+    return await user_service.get_user_by_login(login)
 
 
 @router.delete(
@@ -35,11 +35,11 @@ def get_user_by_login(
         Depends(get_admin_by_access_token),
     ],
 )
-def delete_user_by_login(
+async def delete_user_by_login(
     login: str,
     user_service: Annotated[
         UserService,
         Depends(get_user_service),
     ],
 ):
-    user_service.delete_user_by_login(login)
+    await user_service.delete_user_by_login(login)
