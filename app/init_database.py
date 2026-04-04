0a8019eb99bfa2852bdfcaff4fe8975586aa9ce5
Nikolay <shirkol2006@mail.ru>
2026-04-04 23:56:31 +0300

    Refactor code.

---
+++ app/init_database.py
@@ -0,0 +1,19 @@
+from database import session_factory
+from schemas.user import UserCreate, UserResponse
+from services import UserService
+
+
+async def init_admin():
+    async with session_factory() as session:
+        user_service = UserService(session)
+        if not await user_service.user_login_exists("admin"):
+            create_user_data = UserCreate(
+                surname="admin",
+                name="admin",
+                login="admin",
+                email="admin@admin.gmail.ru",
+                password="admin",
+            )
+            await user_service.create_user(create_user_data)
+            admin: UserResponse = await user_service.get_user_by_login("admin")
+            await user_service.make_admin(admin.id)
