78385098c9e94a7bc12f1723c19c2db437fb6491
Nikolay <shirkol2006@mail.ru>
2026-04-07 16:29:41 +0300

    Add use exceptions in user service.

--- app/services/user.py
+++ app/services/user.py
@@ -13,6 +13,14 @@ from schemas.user import (
     UserLogin,
 )
 
+from core.exceptions.auth import InvalidPasswordError
+from core.exceptions.user import (
+    UserIdNotFoundError,
+    UserLoginNotFoundError,
+    UserLoginAlreadyExistsError,
+    UserEmailAlreadyExistsError,
+)
+
 
 class UserService:
     def __init__(self, session: AsyncSession) -> None:
@@ -24,20 +32,14 @@ class UserService:
         if user is not None:
             return UserResponse.model_validate(user)
 
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"User with {user_id=} not found",
-        )
+        raise UserIdNotFoundError(user_id)
 
     async def get_user_by_login(self, login: str) -> UserResponse:
         user = await self.user_repository.get_user_by_login(login)
         if user is not None:
             return UserResponse.model_validate(user)
 
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"User with {login=} not found",
-        )
+        raise UserLoginNotFoundError(login)
 
     async def user_login_exists(self, login: str) -> bool:
         return await self.user_repository.user_login_exists(login)
@@ -51,16 +53,10 @@ class UserService:
 
     async def create_user(self, create_user_data: UserCreate) -> UserResponse:
         if await self.user_repository.user_login_exists(create_user_data.login):
-            raise HTTPException(
-                status_code=status.HTTP_409_CONFLICT,
-                detail=f"User with login={create_user_data.login} already exists",
-            )
+            raise UserLoginAlreadyExistsError(create_user_data.login)
 
         if await self.user_repository.user_email_exists(create_user_data.email):
-            raise HTTPException(
-                status_code=status.HTTP_409_CONFLICT,
-                detail=f"User with email={create_user_data.email} already exists",
-            )
+            raise UserEmailAlreadyExistsError(create_user_data.email)
 
         create_user_data.password = hash_password(create_user_data.password)
         user = await self.user_repository.create_user(create_user_data)
@@ -68,10 +64,7 @@ class UserService:
 
     async def make_admin(self, user_id: int) -> None:
         if not await self.user_repository.make_admin(user_id):
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail=f"User with id={user_id} not found",
-            )
+            raise UserIdNotFoundError(user_id)
 
     async def update_user(
         self,
@@ -80,28 +73,19 @@ class UserService:
     ) -> UserResponse:
         user = await self.user_repository.get_user_by_id(user_id)
         if user is None:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail=f"User with id={user_id} not found",
-            )
+            raise UserIdNotFoundError(user_id)
 
         if (
             user.login != update_data.login
             and await self.user_repository.user_login_exists(update_data.login)
         ):
-            raise HTTPException(
-                status_code=status.HTTP_409_CONFLICT,
-                detail=f"User with login={update_data.login} already exists",
-            )
+            raise UserLoginAlreadyExistsError(update_data.login)
 
         if (
             user.email != update_data.email
             and await self.user_repository.user_email_exists(update_data.email)
         ):
-            raise HTTPException(
-                status_code=status.HTTP_409_CONFLICT,
-                detail=f"User with email={update_data.email} already exists",
-            )
+            raise UserEmailAlreadyExistsError(update_data.email)
 
         update_data.password = hash_password(update_data.password)
         updated_user = await self.user_repository.update_user(user_id, update_data)
@@ -114,30 +98,21 @@ class UserService:
     ) -> UserResponse:
         user = await self.user_repository.get_user_by_id(user_id)
         if user is None:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail=f"User with id={user_id} not found",
-            )
+            raise UserIdNotFoundError(user_id)
 
         if (
             "login" in update_data.model_fields_set
             and user.login != update_data.login
             and await self.user_repository.user_login_exists(update_data.login)
         ):
-            raise HTTPException(
-                status_code=status.HTTP_409_CONFLICT,
-                detail=f"User with login={update_data.login} already exists",
-            )
+            raise UserLoginAlreadyExistsError(update_data.login)
 
         if (
             "email" in update_data.model_fields_set
             and user.email != update_data.email
             and await self.user_repository.user_email_exists(update_data.email)
         ):
-            raise HTTPException(
-                status_code=status.HTTP_409_CONFLICT,
-                detail=f"User with email={update_data.email} already exists",
-            )
+            raise UserEmailAlreadyExistsError(update_data.email)
 
         if update_data.password is not None:
             update_data.password = hash_password(update_data.password)
@@ -148,40 +123,25 @@ class UserService:
 
     async def delete_user_by_id(self, user_id: int) -> None:
         if not await self.user_repository.delete_user_by_id(user_id):
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail=f"User with id={user_id} not found",
-            )
+            raise UserIdNotFoundError(user_id)
 
     async def delete_user_by_login(self, login: str) -> None:
         if not await self.user_repository.delete_user_by_login(login):
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail=f"User with login={login} not found",
-            )
+            raise UserLoginNotFoundError(login)
 
     async def authenticate_user(self, login_data: UserLogin) -> UserResponse:
         user = await self.user_repository.get_user_by_login(login_data.login)
         if user is None:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail=f"Invalid login",
-            )
+            raise UserLoginNotFoundError(login_data.login)
 
         if not verify_password(login_data.password, user.encrypted_password):
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Invalid password",
-            )
+            raise InvalidPasswordError()
 
         return UserResponse.model_validate(user)
 
     async def is_admin(self, user_id: int) -> bool:
         role = await self.user_repository.get_user_role(user_id)
         if role is None:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail=f"User with id={user_id} not found",
-            )
+            raise UserIdNotFoundError(user_id)
 
         return role == UserRole.admin.value
