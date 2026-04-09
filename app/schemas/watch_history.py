51d583ba83dfc7ed98c8e84e9585b32a9cc1e731
Nikolay <shirkol2006@mail.ru>
2026-04-09 22:59:08 +0300

    Create watch_history schemas.

---
+++ app/schemas/watch_history.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+
+class WatchHistoryBase(BaseModel):
+    """
+    Базовая модель для отображения просмотра фильма пользователем.
+    """
+
+    movie_id: int
+
+
+class WatchHistoryCreate(WatchHistoryBase):
+    """
+    Модель для создания записи о просмотре фильма пользователем.
+    """
+
+
+class WatchHistoryResponse(WatchHistoryBase):
+    """
+    Модель для вывода информации о просмотре фильма пользователем в истории.
+    """
+
+    id: int
+    user_id: int
+    watched_at: datetime
+
+
+class WatchHistoryResponseList(BaseModel):
+    """
+    Модель для вывода истории просмотров фильмов пользователем.
+    """
+
+    watch_history_list: list[WatchHistoryResponse]
