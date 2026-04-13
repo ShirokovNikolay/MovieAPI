1bba238ef94b7c40b50adc76cdc9205774dee3d8
Nikolay <shirkol2006@mail.ru>
2026-04-13 09:53:53 +0300

    Refactor code: add rate limit to all movies endpoints.

--- app/api/api_v1/movies/__init__.py
+++ app/api/api_v1/movies/__init__.py
@@ -1,12 +1,15 @@
 __all__ = ("router",)
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from dependencies.redis import rate_limit_movie
 from .list_views import router as list_movies_router
 from .details_views import router as details_movies_router
 
 router = APIRouter(
     tags=["Movies"],
     prefix="/movies",
+    dependencies=[Depends(rate_limit_movie)],
 )
 
 
