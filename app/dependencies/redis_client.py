629ee16629cff0c55059515a71a50cb2d661fec5
Nikolay <shirkol2006@mail.ru>
2026-04-13 22:39:43 +0300

    Prepare work with dependencies for cache service.

--- app/dependencies/redis_client.py
+++ app/dependencies/redis_client.py
@@ -21,8 +21,29 @@ def redis_client_factory(
 
 
 get_redis_client_for_rate_limiter = redis_client_factory(
-    host=settings.redis.connection.host,
-    port=settings.redis.connection.port,
     db=settings.redis.db.rate_limiter,
-    decode_responses=True,
+)
+
+get_redis_client_for_genres = redis_client_factory(
+    db=settings.redis.db.genres,
+)
+
+get_redis_client_for_movies = redis_client_factory(
+    db=settings.redis.db.movies,
+)
+
+get_redis_client_for_users = redis_client_factory(
+    db=settings.redis.db.users,
+)
+
+get_redis_client_for_reviews = redis_client_factory(
+    db=settings.redis.db.reviews,
+)
+
+get_redis_client_for_favorite_movies = redis_client_factory(
+    db=settings.redis.db.favorite_movies,
+)
+
+get_redis_client_for_watch_history = redis_client_factory(
+    db=settings.redis.db.watch_history,
 )
