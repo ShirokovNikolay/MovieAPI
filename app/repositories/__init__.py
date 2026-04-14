5a536c84a73a7d2ff61cbcb042ba8da87416a449
Nikolay <shirkol2006@mail.ru>
2026-04-14 23:19:45 +0300

    Small fixes.

--- app/repositories/__init__.py
+++ app/repositories/__init__.py
@@ -2,10 +2,14 @@ from .genre import GenreRepository
 from .movie import MovieRepository
 from .review import ReviewRepository
 from .user import UserRepository
+from .favorite_movie import FavoriteMovieRepository
+from .watch_history import WatchHistoryRepository
 
 __all__ = (
     "GenreRepository",
     "MovieRepository",
     "ReviewRepository",
     "UserRepository",
+    "FavoriteMovieRepository",
+    "WatchHistoryRepository",
 )
--- app/services/__init__.py
+++ app/services/__init__.py
@@ -2,10 +2,14 @@ from .genre import GenreService
 from .movie import MovieService
 from .review import ReviewService
 from .user import UserService
+from .favorite_movie import FavoriteMovieService
+from .watch_history import WatchHistoryService
 
 __all__ = (
     "GenreService",
     "MovieService",
     "ReviewService",
     "UserService",
+    "FavoriteMovieService",
+    "WatchHistoryService",
 )
