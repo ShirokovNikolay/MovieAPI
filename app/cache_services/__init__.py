7f6fad286adc8d85e6b06c55890e82cca56cf9db
Nikolay <shirkol2006@mail.ru>
2026-04-16 09:50:24 +0300

    Create review cache service.

--- app/cache_services/__init__.py
+++ app/cache_services/__init__.py
@@ -1,7 +1,9 @@
 from .genre import GenreCacheService
 from .movie import MovieCacheService
+from .review import ReviewCacheService
 
 __all__ = (
     "GenreCacheService",
     "MovieCacheService",
+    "ReviewCacheService",
 )
