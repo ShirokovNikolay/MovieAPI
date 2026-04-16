6199152d2396e3d230be012c8f0adbd9b7da3ba6
Nikolay <shirkol2006@mail.ru>
2026-04-16 18:06:09 +0300

    Create user cache service.

--- app/cache_services/__init__.py
+++ app/cache_services/__init__.py
@@ -2,10 +2,12 @@ from .genre import GenreCacheService
 from .movie import MovieCacheService
 from .review import ReviewCacheService
 from .favorite_movie import FavoriteMovieCacheService
+from .user import UserCacheService
 
 __all__ = (
     "GenreCacheService",
     "MovieCacheService",
     "ReviewCacheService",
     "FavoriteMovieCacheService",
+    "UserCacheService",
 )
