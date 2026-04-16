dae445553dbb36de2cf287f4c62f987cb9939c6d
Nikolay <shirkol2006@mail.ru>
2026-04-16 18:06:09 +0300

    Create user cache service.

---
+++ app/cache_services/user.py
@@ -0,0 +1,108 @@
+from core.security.cache_utils import create_cache_key
+from redis_cache import CacheService
+from schemas.user import (
+    UserResponse,
+    UserResponseList,
+    UserCreate,
+    UserUpdate,
+    UserPartialUpdate,
+    UserLogin,
+)
+from services import UserService
+
+
+class UserCacheService:
+    def __init__(
+        self,
+        user_service: UserService,
+        cache_service: CacheService,
+    ):
+        self.user_service = user_service
+        self.cache_service = cache_service
+
+    async def get_user_by_id(self, user_id: int) -> UserResponse:
+        key = create_cache_key("user", user_id=user_id)
+        cached_user_response = await self.cache_service.get(key, UserResponse)
+        if cached_user_response is not None:
+            return cached_user_response
+
+        user_response = await self.user_service.get_user_by_id(user_id)
+        await self.cache_service.set(key, user_response)
+        return user_response
+
+    async def get_user_by_login(self, login: str) -> UserResponse:
+        key = create_cache_key("user", login=login)
+        cached_user_response = await self.cache_service.get(key, UserResponse)
+        if cached_user_response is not None:
+            return cached_user_response
+
+        user_response = await self.user_service.get_user_by_login(login)
+        await self.cache_service.set(key, user_response)
+        return user_response
+
+    async def get_all_users(self, size: int = 10, page: int = 1) -> UserResponseList:
+        key = create_cache_key("users", size=size, page=page)
+        cached_users_response = await self.cache_service.get(key, UserResponse)
+        if cached_users_response is not None:
+            return cached_users_response
+
+        users_response = await self.user_service.get_all_users(size, page)
+        await self.cache_service.set(key, users_response)
+        return users_response
+
+    async def create_user(self, create_user_data: UserCreate) -> UserResponse:
+        user_response = await self.user_service.create_user(create_user_data)
+        key = create_cache_key("user")
+        pattern = key + "*"
+        await self.cache_service.delete_by_pattern(pattern)
+        return user_response
+
+    async def update_user(
+        self,
+        user_id: int,
+        update_data: UserUpdate,
+    ) -> UserResponse:
+        user_response = await self.user_service.update_user(user_id, update_data)
+        key = create_cache_key("user")
+        pattern = key + "*"
+        await self.cache_service.delete_by_pattern(pattern)
+        return user_response
+
+    async def partial_update_user(
+        self,
+        user_id: int,
+        update_data: UserPartialUpdate,
+    ) -> UserResponse:
+        user_response = await self.user_service.partial_update_user(
+            user_id, update_data
+        )
+        key = create_cache_key("user")
+        pattern = key + "*"
+        await self.cache_service.delete_by_pattern(pattern)
+        return user_response
+
+    async def delete_user_by_id(self, user_id: int) -> None:
+        await self.user_service.delete_user_by_id(user_id)
+        key = create_cache_key("user")
+        pattern = key + "*"
+        await self.cache_service.delete_by_pattern(pattern)
+
+    async def delete_user_by_login(self, login: str) -> None:
+        await self.user_service.delete_user_by_login(login)
+        key = create_cache_key("user")
+        pattern = key + "*"
+        await self.cache_service.delete_by_pattern(pattern)
+
+    async def authenticate_user(self, login_data: UserLogin) -> UserResponse:
+        key = create_cache_key(
+            "user",
+            login=login_data.login,
+            password=login_data.password,
+        )
+        cached_user_response = await self.cache_service.get(key, UserResponse)
+        if cached_user_response is not None:
+            return cached_user_response
+
+        user_response = await self.user_service.authenticate_user(login_data)
+        await self.cache_service.set(key, user_response)
+        return user_response
