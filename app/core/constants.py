430013375d4917496e85626f4dcf0f5fe5355e54
Nikolay <shirkol2006@mail.ru>
2026-04-23 18:05:47 +0300

    Create tests on base exceptions.

--- app/core/constants.py
+++ app/core/constants.py
@@ -1,9 +1,25 @@
 from enum import StrEnum
 
+from core.exceptions.base import (
+    AuthenticationError,
+    ConflictError,
+    ForbiddenError,
+    NotFoundError,
+    TooManyRequestsError,
+)
+
 TOKEN_TYPE: str = "type"
 ACCESS_TOKEN_TYPE: str = "access"
 REFRESH_TOKEN_TYPE: str = "refresh"
 BEARER_TOKEN_TYPE: str = "Bearer"
+
+BASE_ERROR = (
+    NotFoundError
+    | ConflictError
+    | ForbiddenError
+    | AuthenticationError
+    | TooManyRequestsError
+)
 
 
 class UserRole(StrEnum):
