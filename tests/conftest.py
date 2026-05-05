519a75285e8adbc9fac31e2a64ce0275f06eb936
Nikolay Shirokov <shirkol2006@mail.ru>
2026-05-05 10:20:27 +0300

    Configure event loop in pytest.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,15 +2,10 @@ from os import getenv
 from typing import AsyncGenerator
 
 import pytest
-from sqlalchemy.ext.asyncio import (
-    AsyncSession,
-    create_async_engine,
-    async_sessionmaker,
-    AsyncEngine,
-)
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
-from core.config import settings
+from core.database.connection import session_factory
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -22,21 +17,7 @@ def test_environment_is_ready() -> None:
 
 
 @pytest.fixture(scope="function")
-async def engine() -> AsyncEngine:
-    return create_async_engine(
-        url=settings.database.url_database,
-    )
-
-
-@pytest.fixture(scope="function")
-async def session_factory(engine: AsyncEngine) -> async_sessionmaker:
-    return async_sessionmaker(bind=engine)
-
-
-@pytest.fixture(scope="function")
-async def session(
-    session_factory: async_sessionmaker,
-) -> AsyncGenerator[AsyncSession, None]:
+async def session() -> AsyncGenerator[AsyncSession, None]:
     async with session_factory() as database_session:
         yield database_session
         await database_session.rollback()
