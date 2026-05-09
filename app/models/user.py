79a6284f6cf274f83024b753aa64af864a52a0ec
Nikolay Shirokov <shirkol2006@mail.ru>
2026-05-09 15:25:02 +0300

    Add constraints to user table.

--- app/models/user.py
+++ app/models/user.py
@@ -1,10 +1,17 @@
 from datetime import datetime
 from typing import TYPE_CHECKING
 
-from sqlalchemy import Enum, String, func
+from sqlalchemy import CheckConstraint, Enum, String, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from core.constants import UserRole
+from core.constants import (
+    USER_EMAIL_MAX_LENGTH,
+    USER_ENCRYPTED_PASSWORD_MAX_LENGTH,
+    USER_LOGIN_MAX_LENGTH,
+    USER_NAME_MAX_LENGTH,
+    USER_SURNAME_MAX_LENGTH,
+    UserRole,
+)
 from core.database.connection import Base
 
 if TYPE_CHECKING:
@@ -21,11 +28,13 @@ class User(Base):
         Enum(UserRole, name="user_role"),
         server_default=UserRole.user.value,
     )
-    surname: Mapped[str] = mapped_column(String(30))
-    name: Mapped[str] = mapped_column(String(20))
-    login: Mapped[str] = mapped_column(String(20))
-    email: Mapped[str] = mapped_column(String(255))
-    encrypted_password: Mapped[str] = mapped_column(String(128))
+    surname: Mapped[str] = mapped_column(String(USER_SURNAME_MAX_LENGTH))
+    name: Mapped[str] = mapped_column(String(USER_NAME_MAX_LENGTH))
+    login: Mapped[str] = mapped_column(String(USER_LOGIN_MAX_LENGTH))
+    email: Mapped[str] = mapped_column(String(USER_EMAIL_MAX_LENGTH))
+    encrypted_password: Mapped[str] = mapped_column(
+        String(USER_ENCRYPTED_PASSWORD_MAX_LENGTH),
+    )
     registration_date: Mapped[datetime] = mapped_column(server_default=func.now())
     reviews: Mapped[list["Review"]] = relationship(
         "Review",
@@ -38,4 +47,11 @@ class User(Base):
     watch_history: Mapped[list["WatchHistory"]] = relationship(
         "WatchHistory",
         back_populates="user",
+    )
+
+    __table_args__ = (
+        CheckConstraint("LENGTH(surname) >= 3 AND LENGTH(surname) <= 30"),
+        CheckConstraint("LENGTH(name) >= 3 AND LENGTH(name) <= 20"),
+        CheckConstraint("LENGTH(login) >= 3 AND LENGTH(login) <= 20"),
+        CheckConstraint("LENGTH(email) >= 10 AND LENGTH(email) <= 40"),
     )
