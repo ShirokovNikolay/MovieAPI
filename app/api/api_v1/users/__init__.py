91f2e760c10039e75d6a83aae9929185bd3cc81a
Nikolay Shirokov <shirkol2006@mail.ru>
2026-05-10 21:17:23 +0300

    Refactor code: rework user views structure.

--- app/api/api_v1/users/__init__.py
+++ app/api/api_v1/users/__init__.py
@@ -4,8 +4,6 @@ from fastapi import APIRouter, Depends
 from dependencies.rate_limiter import check_rate_limit_auth
 
 from .details_views import router as details_users_router
-from .details_views_login import router as details_users_router_login
-from .list_views import router as list_users_router
 from .profile_views import router as profile_users_router
 
 router = APIRouter(
@@ -16,8 +14,4 @@ router = APIRouter(
     ],
 )
 router.include_router(profile_users_router)
-router_by_login = APIRouter(prefix="/by-login")
-router_by_login.include_router(details_users_router_login)
-router.include_router(list_users_router)
 router.include_router(details_users_router)
-router.include_router(router_by_login)
