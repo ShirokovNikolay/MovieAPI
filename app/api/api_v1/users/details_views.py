744675b974a71f5be8d593ede1d503803bd63dd8
Nikolay Shirokov <shirkol2006@mail.ru>
2026-05-10 21:17:23 +0300

    Refactor code: rework user views structure.

--- app/api/api_v1/users/details_views.py
+++ app/api/api_v1/users/details_views.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter, Depends, status
 
 from dependencies.annotations.cache_services import UserCacheServiceDep
+from dependencies.annotations.validators import PaginationPageDep, PaginationSizeDep
 from dependencies.auth import get_admin_by_access_token
-from schemas.user import UserResponse
+from schemas.user import UserResponse, UserResponseList
 
 router = APIRouter(
-    prefix="/{user_id}",
     dependencies=[
         Depends(get_admin_by_access_token),
     ],
@@ -14,6 +14,19 @@ router = APIRouter(
 
 @router.get(
     "/",
+    response_model=UserResponseList,
+    status_code=status.HTTP_200_OK,
+)
+async def get_users(
+    user_cache_service: UserCacheServiceDep,
+    size: PaginationSizeDep = 10,
+    page: PaginationPageDep = 1,
+) -> UserResponseList:
+    return await user_cache_service.get_all_users(size, page)
+
+
+@router.get(
+    "/{user_id}",
     response_model=UserResponse,
     status_code=status.HTTP_200_OK,
 )
@@ -25,7 +38,7 @@ async def get_user_by_id(
 
 
 @router.delete(
-    "/",
+    "/{user_id}",
     status_code=status.HTTP_204_NO_CONTENT,
 )
 async def delete_user_by_id(
@@ -33,3 +46,26 @@ async def delete_user_by_id(
     user_cache_service: UserCacheServiceDep,
 ) -> None:
     await user_cache_service.delete_user_by_id(user_id)
+
+
+@router.get(
+    "/login/{login}",
+    response_model=UserResponse,
+    status_code=status.HTTP_200_OK,
+)
+async def get_user_by_login(
+    login: str,
+    user_cache_service: UserCacheServiceDep,
+) -> UserResponse:
+    return await user_cache_service.get_user_by_login(login)
+
+
+@router.delete(
+    "/login/{login}",
+    status_code=status.HTTP_204_NO_CONTENT,
+)
+async def delete_user_by_login(
+    login: str,
+    user_cache_service: UserCacheServiceDep,
+) -> None:
+    await user_cache_service.delete_user_by_login(login)
