d7ccfbd8cb1cc8ac063d0be0d9cda00bad682c9e
Nikolay Shirokov <shirkol2006@mail.ru>
2026-05-10 21:00:48 +0300

    Clean code from useless views.

--- app/api/api_v1/users/list_views.py
+++ app/api/api_v1/users/list_views.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends, status
 from dependencies.annotations.cache_services import UserCacheServiceDep
 from dependencies.annotations.validators import PaginationPageDep, PaginationSizeDep
 from dependencies.auth import get_admin_by_access_token
-from schemas.user import UserCreate, UserResponse, UserResponseList
+from schemas.user import UserResponseList
 
 router = APIRouter(
     dependencies=[
@@ -23,15 +23,3 @@ async def get_users(
     page: PaginationPageDep = 1,
 ) -> UserResponseList:
     return await user_cache_service.get_all_users(size, page)
-
-
-@router.post(
-    "/",
-    response_model=UserResponse,
-    status_code=status.HTTP_201_CREATED,
-)
-async def create_user(
-    create_user_data: UserCreate,
-    user_cache_service: UserCacheServiceDep,
-) -> UserResponse:
-    return await user_cache_service.create_user(create_user_data)
