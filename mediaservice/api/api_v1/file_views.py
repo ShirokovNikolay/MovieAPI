e16bd0de274c19db4dd2024829a54d50302bdb0f
Nikolay Shirokov <shirkol2006@mail.ru>
2026-06-01 19:15:54 +0300

    Refactor code.

--- mediaservice/api/api_v1/file_views.py
+++ mediaservice/api/api_v1/file_views.py
@@ -1,4 +1,5 @@
 import uuid
+from enum import StrEnum
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, UploadFile, status
@@ -15,30 +16,57 @@ class ConfirmUploadRequest(BaseModel):
     dest_path: str
 
 
-@router.get(
+class S3Bucket(StrEnum):
+    genre_posters = "genre-posters"
+    movie_posters = "movie-posters"
+    movies = "movies"
+
+
+class S3ContentType(StrEnum):
+    jpeg = "image/jpeg"
+    jpg = "image/jpg"
+    pngr = "image/png"
+    webp = "image/webp"
+    mp4 = "video/mp4"
+
+
+class CreatePresignedUrlRequest(BaseModel):
+    bucket_name: S3Bucket
+    file_name: str
+    content_type: S3ContentType
+
+
+class PresignUrlResponse(BaseModel):
+    url: str
+    path: str
+
+
+MinioServiceDep = Annotated[
+    MinioService,
+    Depends(get_minio_service),
+]
+
+
+@router.post(
     "/presign-url",
     status_code=status.HTTP_200_OK,
 )
 async def get_presign_url(
-    file_name: str,
-    content_type: str,
-    minio_service: Annotated[
-        MinioService,
-        Depends(get_minio_service),
-    ],
-) -> dict[str, str]:
-    file_path = f"tmp/genre/{uuid.uuid4()}_{file_name}"
+    presign_request: CreatePresignedUrlRequest,
+    minio_service: MinioServiceDep,
+) -> PresignUrlResponse:
+    file_path = f"tmp/genre/{uuid.uuid4()}_{presign_request.file_name}"
     url = await minio_service.create_presigned_url(
-        bucket_name="genre-posters",
+        bucket_name=presign_request.bucket_name.value,
         object_name=file_path,
         expires_in=15 * 60,
         client_method="put_object",
-        content_type=content_type,
+        content_type=presign_request.content_type.value,
     )
-    return {
-        "upload_url": url,
-        "temp_path": file_path,
-    }
+    return PresignUrlResponse(
+        url=url,
+        path=file_path,
+    )
 
 
 @router.post(
@@ -47,7 +75,7 @@ async def get_presign_url(
 )
 async def confirm_upload(
     payload: ConfirmUploadRequest,
-    minio_service: Annotated[MinioService, Depends(get_minio_service)],
+    minio_service: MinioServiceDep,
 ) -> None:
     await minio_service.move_file(
         bucket_name="genre-posters",
@@ -56,17 +84,17 @@ async def confirm_upload(
     )
 
 
-@router.post("/upload", status_code=status.HTTP_200_OK)
+@router.post(
+    "/upload",
+    status_code=status.HTTP_200_OK,
+)
 async def upload_file(
     file: UploadFile,
-    minio_service: Annotated[
-        MinioService,
-        Depends(get_minio_service),
-    ],
+    minio_service: MinioServiceDep,
 ) -> None:
     file_path = f"genre/{uuid.uuid4()}_{file.filename}"
     await minio_service.upload_file(
-        bucket="genre-posters",
+        bucket_name="genre-posters",
         file=file.file,
         key=file_path,
     )
