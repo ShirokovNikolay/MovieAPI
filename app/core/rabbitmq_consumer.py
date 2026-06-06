7054bc83cdf547ead6aebde5d0c9ed6ab4197f57
Nikolay Shirokov <shirkol2006@mail.ru>
2026-06-06 21:46:06 +0300

    Refactor code: use asynccontextmanager to get genre cache service in callback update_genre_url.

--- app/core/rabbitmq_consumer.py
+++ app/core/rabbitmq_consumer.py
@@ -1,51 +1,70 @@
 import json
+from collections.abc import AsyncGenerator
+from contextlib import asynccontextmanager
 
 from aio_pika import IncomingMessage, Message
-from packages.rabbit_mq import connection, get_rabbit_mq_service
+from packages.rabbit_mq import RabbitMQService, connection
 
+from cache_services import GenreCacheService
+from core.config import settings
 from core.constants import BASE_MINIO_URL
 from core.database import session_factory
-from dependencies.services import get_genre_service
+from core.redis import CacheService
+from dependencies.redis_client import redis_client_factory
 from schemas.genre import GenrePartialUpdate
+from services import GenreService
+
+
+@asynccontextmanager
+async def get_genre_cache_service() -> AsyncGenerator[GenreCacheService]:
+    async with (
+        session_factory() as session,
+        connection.RABBIT_MQ_CONNECTION.channel() as channel,  # type: ignore[union-attr]
+    ):
+        rabbit_mq_service = RabbitMQService(channel)
+        genre_service = GenreService(session, rabbit_mq_service)
+        get_redis_client_for_genres = redis_client_factory(
+            db=settings.redis.db.genres,
+        )
+        async for redis_client in get_redis_client_for_genres():
+            cache_service = CacheService(redis_client)
+            genre_cache_service = GenreCacheService(
+                genre_service=genre_service,
+                cache_service=cache_service,
+            )
+            yield genre_cache_service
 
 
 async def update_genre_url(message: IncomingMessage) -> None:
-    async with message.process():
-        # print("updating genre url")
-        channel = await connection.RABBIT_MQ_CONNECTION.channel()  # type: ignore[union-attr]
-        async for rabbitmq_service in get_rabbit_mq_service(channel):
-            async for genre_service in get_genre_service(
-                session=session_factory(),
-                rabbitmq=rabbitmq_service,
-            ):
-                data = json.loads(message.body.decode())
-                genre_id, bucket_name, object_name, new_object_name = (
-                    data["genre_id"],
-                    data["bucket_name"],
-                    data["object_name"],
-                    data["new_object_name"],
-                )
-                genre_partial_update = GenrePartialUpdate(
-                    preview_url=BASE_MINIO_URL + "/genre-posters/" + new_object_name,
-                )
-                # print("New object name:", new_object_name)
-                await genre_service.partial_update_genre(
-                    genre_id=genre_id,
-                    update_data=genre_partial_update,
-                )
+    async with message.process(), get_genre_cache_service() as genre_cache_service:
+        data = json.loads(message.body.decode())
+        genre_id, bucket_name, object_name, new_object_name = (
+            data["genre_id"],
+            data["bucket_name"],
+            data["object_name"],
+            data["new_object_name"],
+        )
+        genre_partial_update = GenrePartialUpdate(
+            preview_url=BASE_MINIO_URL + "/genre-posters/" + new_object_name,
+        )
+        await genre_cache_service.partial_update_genre(
+            genre_id=genre_id,
+            update_data=genre_partial_update,
+        )
 
-            exchange = await rabbitmq_service.declare_exchange(
-                name="to_mediaservice",
-                type="direct",
-            )
-            body = {
-                "object_name": object_name,
-                "bucket_name": bucket_name,
-            }
-            await rabbitmq_service.publish(
-                message=Message(
-                    body=json.dumps(body).encode(),
-                ),
-                exchange=exchange,
-                routing_key="to_mediaservice",
-            )
+        rabbitmq_service = genre_cache_service.genre_service.rabbitmq_service
+        exchange = await rabbitmq_service.declare_exchange(
+            name="to_mediaservice",
+            type="direct",
+        )
+        body = {
+            "object_name": object_name,
+            "bucket_name": bucket_name,
+        }
+        await rabbitmq_service.publish(
+            message=Message(
+                body=json.dumps(body).encode(),
+            ),
+            exchange=exchange,
+            routing_key="to_mediaservice",
+        )
