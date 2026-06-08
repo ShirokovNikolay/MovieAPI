2466d3ec31021c0645f0d0d857068e9d7994760f
Nikolay Shirokov <shirkol2006@mail.ru>
2026-06-08 08:13:01 +0300

    Rewrite mediaservice lifespan.

--- packages/rabbitmq/connection.py
+++ packages/rabbitmq/connection.py
@@ -6,14 +6,14 @@ from aio_pika.abc import AbstractChannel
 RABBIT_MQ_CONNECTION = None
 
 
-async def init_rabbit_mq() -> None:
+async def init_rabbitmq() -> None:
     global RABBIT_MQ_CONNECTION  # noqa: PLW0603
     RABBIT_MQ_CONNECTION = await aio_pika.connect_robust(
         url="amqp://guest:guest@rabbitmq:5672/%2f",
     )
 
 
-async def close_rabbit_mq() -> None:
+async def close_rabbitmq() -> None:
     global RABBIT_MQ_CONNECTION  # noqa: PLW0602
     if RABBIT_MQ_CONNECTION is not None:
         await RABBIT_MQ_CONNECTION.close()
