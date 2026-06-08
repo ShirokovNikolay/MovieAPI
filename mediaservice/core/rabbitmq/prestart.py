5f1f79d9c37cab4078a86738b270ba7b7d503eaa
Nikolay Shirokov <shirkol2006@mail.ru>
2026-06-08 14:51:40 +0300

    Rewrite code using constants.

--- mediaservice/core/rabbitmq/prestart.py
+++ mediaservice/core/rabbitmq/prestart.py
@@ -1,5 +1,6 @@
 from collections.abc import AsyncGenerator
 
+from packages.constants import Exchange, ExchangeType, Queue
 from packages.rabbitmq.connection import init_rabbitmq
 from packages.rabbitmq.utils import get_rabbitmq_service
 
@@ -10,22 +11,22 @@ async def start_rabbitmq() -> AsyncGenerator[None]:
     await init_rabbitmq()
     async with get_rabbitmq_service() as rabbitmq_service:
         queue_copy = await rabbitmq_service.declare_queue(
-            "copy_file_queue",
+            name=Queue.copy_file.value,
             durable=True,
         )
         queue_delete = await rabbitmq_service.declare_queue(
-            "delete_tmp_queue",
+            name=Queue.delete_file.value,
             durable=True,
         )
 
         exchange = await rabbitmq_service.declare_exchange(
-            "to_mediaservice",
-            "direct",
+            name=Exchange.app.value,
+            type=ExchangeType.direct.value,
             durable=True,
         )
 
-        await queue_copy.bind(exchange, "to_mediaservice")
-        await queue_delete.bind(exchange, "to_mediaservice")
+        await queue_copy.bind(exchange, Queue.copy_file.value)
+        await queue_delete.bind(exchange, Queue.delete_file.value)
 
         await rabbitmq_service.consume(queue_copy, copy_file)
         await rabbitmq_service.consume(queue_delete, delete_temporary_file)
