54c2f5f8a3cdee2081ee8e915db9697519483740
Nikolay Shirokov <shirkol2006@mail.ru>
2026-06-12 20:20:09 +0300

    Fix circular import.

--- mediaservice/core/celery_core/tasks.py
+++ mediaservice/core/celery_core/tasks.py
@@ -1,13 +1,14 @@
 import asyncio
 
+from ..rabbitmq.utils import get_minio_service
 from .celery_app import app
 
-from ..rabbitmq.utils import get_minio_service
 
-
-@app.task
+@app.task(
+    name="mediaservice.media.delete_temporary_file",
+)
 def delete_temporary_file(bucket_name: str, object_name: str) -> None:
-    async def callback() -> None:
+    async def async_delete_temporary_file() -> None:
         async with get_minio_service() as minio_service:
             if await minio_service.file_exists(bucket_name, object_name):
                 await minio_service.delete_file(
@@ -15,4 +16,6 @@ def delete_temporary_file(bucket_name: str, object_name: str) -> None:
                     key=object_name,
                 )
 
-    asyncio.run(callback())
+    asyncio.run(
+        async_delete_temporary_file(),
+    )
