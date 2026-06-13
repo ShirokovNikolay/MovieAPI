6925f870bc42edd9327289a97c523cabadb6896c
Nikolay Shirokov <shirkol2006@mail.ru>
2026-06-13 07:12:36 +0300

    Refactor code.

--- mediaservice/core/celery/celery_app.py
+++ mediaservice/core/celery/celery_app.py
@@ -1,7 +1,7 @@
 from celery import Celery
 
 app = Celery(
-    "core.celery_core.celery_app",
+    "core.celery.celery_app",
     broker="amqp://guest:guest@rabbitmq:5672/%2f",
-    include=["core.celery_core.tasks"],
+    include=["core.celery.tasks"],
 )
