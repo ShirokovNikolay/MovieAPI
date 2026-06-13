8dc5cc1e7a10d5c2669501b7f6f7faa491915465
Nikolay Shirokov <shirkol2006@mail.ru>
2026-06-13 19:52:02 +0300

    Add EmailService to send email.

---
+++ notification-service/core/config.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+from typing import ClassVar
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    BASE_DIR: Path = Path(__file__).parent.parent
+    mail_host: str = "smtp.yandex.ru"
+    mail_port: int = 587
+    corporate_mail: str = "email"
+    mail_password: str = "password"
+    start_tls: bool = True
+    model_config: ClassVar[SettingsConfigDict] = SettingsConfigDict(
+        case_sensitive=False,
+        env_file=BASE_DIR / ".env",
+        env_nested_delimiter="__",
+    )
+
+
+settings = Settings()
