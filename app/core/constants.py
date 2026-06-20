6450f03cc1eefb1d4d5778355b79152a14fa4367
Nikolay Shirokov <shirkol2006@mail.ru>
2026-06-20 21:59:46 +0300

    Add create temporary jwt tokens for security registration, 2fa, reset password.

--- app/core/constants.py
+++ app/core/constants.py
@@ -59,15 +59,12 @@ class MethodType(StrEnum):
     delete = "DELETE"
 
 
-class MessageType(StrEnum):
-    verify_email = "verify_email"
-    two_factor_auth = "two_factor_auth"
-    reset_password = "reset_password"
-
-
-TOKEN_TYPE: str = "type"
-ACCESS_TOKEN_TYPE: str = "access"
-REFRESH_TOKEN_TYPE: str = "refresh"
+TOKEN_TYPE = "type"
+ACCESS_TOKEN_TYPE = "access"
+REFRESH_TOKEN_TYPE = "refresh"
+REGISTRATION_TEMPORARY_TOKEN_TYPE = "registration_temporary_token"
+TWO_FACTOR_VERIFICATION_TEMPORARY_TOKEN_TYPE = "two_factor_verification_temporary_token"
+RESET_PASSWORD_TEMPORARY_TOKEN_TYPE = "reset_password_temporary_token"
 
 BEARER_TOKEN_TYPE: str = "Bearer"
 
