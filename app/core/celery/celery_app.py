d4e2027534ff87e05660058e73ac0a4d36366e62
Nikolay Shirokov <shirkol2006@mail.ru>
2026-06-26 18:29:13 +0300

    Small fixes.

--- app/core/celery/celery_app.py
+++ app/core/celery/celery_app.py
@@ -1,16 +1,18 @@
 from celery import Celery
+from packages.celery.constants import Queue, TaskType
+from packages.config import settings as package_settings
 
 app = Celery(
     "core.celery.celery_app",
-    broker="amqp://guest:guest@rabbitmq:5672/%2f",
+    broker=package_settings.rabbitmq.rabbitmq_url,
     backend="redis://redis:6379/0",
     include=["core.celery.tasks"],
 )
 
 app.conf.beat_schedule = {
     "run-spam-every-30-seconds": {
-        "task": "testing",
-        "schedule": 5 * 60,
-        "options": {"queue": "movie-catalog"},
+        "task": TaskType.create_chain_user_reminder.value,
+        "schedule": 30,
+        "options": {"queue": Queue.app},
     },
 }
